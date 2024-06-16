48b6c30a1382fafc374244bd4810d967e9c9dcc7
Thales Nishida <medeiros.tn@gmail.com>
2024-06-15 22:34:49 -0300

    first commit

---
+++ src/main/kotlin/br/com/thalesnishida/portifolio/PortifolioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.thalesnishida.portifolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortifolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortifolioApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/thalesnishida/portifolio/PortifolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.thalesnishida.portifolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortifolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
